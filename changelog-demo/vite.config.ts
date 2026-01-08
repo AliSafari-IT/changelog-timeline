834e177f86e397c3bbccc1cc9e9c7a48d0125dc8
Ali Safari <asafarim.it@gmail.com>
2026-01-08 12:32:24 +0100

    fixes to address the GitHub Pages 404 issue.

--- changelog-demo/vite.config.ts
+++ changelog-demo/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: process.env.BASE_PATH || '/changelog-timeline/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5187,
   },
